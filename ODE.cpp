bf82ead424252a1a821153ff49b0e146b9d750f8
AlexanderSierraPulido <40802531+AlexanderSierraPulido@users.noreply.github.com>
2018-11-18 20:55:50 -0500

    Add files via upload

--- ODE.cpp
+++ ODE.cpp
@@ -2,16 +2,32 @@
 #include <fstream>
 #include <math.h>
 
+double vel_x(double c,float vx,float vy, float m)
+    {
+        return (-c*vx*vx)/m;
+    }
+
+double vel_y(float g,double c,float vx,float vy,float m)
+    {
+        return (-g*((-c*vy*vy)/m));
+    }
+
 
 int main()
 {
 
+//Condiciones iniciales
+int N=17500;
+int anglemax=70;
+double c=0.2;
+float g=10.0;
+float m=0.2;
+float v=300;
+float angle=45;
+float xi=0;
+float vxin=v*cos(angle);
+float vyin=v*sin(angle);
 
 
-
-
-
-
-
-
+return 0;
 }
--- PDE.cpp
+++ PDE.cpp
@@ -5,9 +5,14 @@
 
 int main()
 {
-
-
-
-
+    
+//Condiciones iniciales
+int N=100000;
+int tini_vari=100.0;
+int tini_calc=10;
+double cte=1.62/(820*2.71);
+double t[50];
+float delta=1.0;
+float dt=0.001;
 
 }
